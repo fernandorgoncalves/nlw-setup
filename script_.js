5ef0410cf95ee847d156c3b74cc1e766d4860299
Fernando <frocha@cibergestion.com.br>
2023-01-20 21:03:53 -0300

    initial commit

---
+++ script_.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NlwSetup(form)
+
+const data = {
+  run: ["01-01", "01-02", "01-06"],
+}
+nlwSetup.setData(data)
+nlwSetup.load()
