a198fa359e8ebb45514de93dc456defd2f6f8eea
Fernando <fernandorgoncalves@hotmail.com>
2023-01-20 21:07:08 -0300

    Delete script_.js

--- script_.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector("#form-habits")
-const nlwSetup = new NlwSetup(form)
-
-const data = {
-  run: ["01-01", "01-02", "01-06"],
-}
-nlwSetup.setData(data)
-nlwSetup.load()
